986ee3a54555d9ffaa8a20931b9616c0f32d4b65
luc perrot <luc.perrot@hotmail.fr>
2023-05-24 15:55:21 +0200

    Exercice 2 Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Seeding..."
+Restaurant.create(name: 'La Petite Boulangerie', category: 'chinese', address: '123 Rue de la Gastronomie, Villeville', phone_number: '+1 555-1234')
+Restaurant.create(name: 'Le Jardin Secret', category: 'italian', address: '456 Avenue des Saveurs, Citéville', phone_number: '+44 20 5555 6789')
+Restaurant.create(name: "L'Auberge du Bonheur", category: 'japanese', address: '789 Boulevard de la Bonne Cuisine, Gourmandville', phone_number: '+61 2 5555 4321')
+Restaurant.create(name: 'La Table Enchantée', category: 'french', address: '101 Rue de la Délicatesse, Savoureuxbourg', phone_number:'+33 1 5555 9876')
+Restaurant.create(name: 'Le Grill de la Mer', category: 'belgian', address: '222 Avenue du Goût, Gourmetville', phone_number:'+49 30 5555 8765')
+puts "Seeding done."
